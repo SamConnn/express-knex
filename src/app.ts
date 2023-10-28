2112f6598f1194bafb4f2e5e81e3989bb32fe906
dinal.infinity <dinal.infinity@infinity.com>
2023-10-28 10:58:31 +0700

    updated

--- src/app.ts
+++ src/app.ts
@@ -8,26 +8,22 @@ import helmet from 'helmet'
 import hpp from 'hpp'
 import morgan from 'morgan'
 import path from 'path'
-import logger from './lib/logger/logger'
-import AppError from './utils/appError'
 
 import { errorHandler, notFoundHandler } from './lib/handlers'
+import logger from './lib/logger/logger'
 import { routes } from './route'
+import AppError from './utils/appError'
 import useRoutes from './utils/route'
 
 const app = express()
 
 app.use(cors())
-
 app.options('*', cors())
 // app.options('/api/v1/tours/:id', cors());
-
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')))
-
 // Set security HTTP headers
 app.use(helmet())
-
 // Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
@@ -39,23 +35,16 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!'
 })
 app.use('/api', limiter)
-
 app.use(bodyParser.json({ limit: '10kb' }))
-
 app.use(express.json({ limit: '10kb' }))
 app.use(express.urlencoded({ extended: true, limit: '10kb' }))
 app.use(cookieParser())
-
 app.use(hpp({}))
-
 app.use(compression())
-
 useRoutes(app, routes)
-
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
 })
-
 app.use(notFoundHandler)
 //Error Handler
 app.use(logger.requestLogger)
