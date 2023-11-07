199dee940d46e4fc4568353ba5b7335690e59b79
dinal.infinity <dinal.infinity@infinity.com>
2023-11-07 11:48:22 +0700

    refactor code

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,7 +1,7 @@
-import { type NextFunction, type Request, type Response } from 'express'
+import { type NextFunction, type Request } from 'express'
 import { BadRequestError } from '../lib/errors'
 
-export const validate = (schema: any) => async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+export const validateRequest = (schema: any) => async (req: Request, next: NextFunction): Promise<any> => {
   try {
     await schema.validate({
       body: req.body,
--- src/route/user.route.ts
+++ src/route/user.route.ts
@@ -2,7 +2,7 @@
 import express from 'express'
 import authController from '../controller/auth.controller'
 import userController from '../controller/user.controller'
-import { validate } from '../middleware/validate'
+import { validateRequest } from '../middleware/validate'
 import { userSchema } from '../utils/validator'
 
 const route = express.Router()
@@ -15,8 +15,8 @@ route.use(authController.protect, authController.restrictTo('Admin'))
 const routes = [
   { method: 'get', path: '/', middleware: [], controller: userController.getUser },
   { method: 'get', path: '/:id', middleware: [], controller: userController.findUserById },
-  { method: 'post', path: '/', middleware: [validate(userSchema)], controller: userController.createUser },
-  { method: 'put', path: '/:id', middleware: [validate(userSchema)], controller: userController.updateUser },
+  { method: 'post', path: '/', middleware: [validateRequest(userSchema)], controller: userController.createUser },
+  { method: 'put', path: '/:id', middleware: [validateRequest(userSchema)], controller: userController.updateUser },
   { method: 'delete', path: '/:id', middleware: [], controller: userController.deleteUser }
 ]
 
